b92e620423bba6a32ef5337090b0af852f23eb73
modmoto <simonheiss87@gmail.com>
2023-05-17 22:47:29 +0200

    fix ex install path

--- setup-tex-live.sh
+++ setup-tex-live.sh
@@ -8,5 +8,5 @@ wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 zcat install-tl-unx.tar.gz | tar xf -
 perl /tmp/install-tl-*/install-tl --no-interaction --no-doc-install --no-src-install --scheme=small
 
-/usr/local/texlive/2022/bin/x86_64-linux/tlmgr init-usertree
-/usr/local/texlive/2022/bin/x86_64-linux/tlmgr install titlesec
+/usr/local/texlive/2023/bin/x86_64-linux/tlmgr init-usertree
+/usr/local/texlive/2023/bin/x86_64-linux/tlmgr install titlesec
